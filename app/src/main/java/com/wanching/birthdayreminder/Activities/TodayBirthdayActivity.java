eab692284fda4532760d6b5c97f28f8cd127a4fd
Wan Ching <wanching_97@hotmail.com>
2017-08-17 20:46:13 +0800

    added setEmptyView

--- app/src/main/java/com/wanching/birthdayreminder/Activities/TodayBirthdayActivity.java
+++ app/src/main/java/com/wanching/birthdayreminder/Activities/TodayBirthdayActivity.java
@@ -5,8 +5,11 @@ import android.database.Cursor;
 import android.support.v4.widget.CursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.wanching.birthdayreminder.Adapters.BirthdayCursorAdapter;
 import com.wanching.birthdayreminder.R;
@@ -21,6 +24,9 @@ public class TodayBirthdayActivity extends AppCompatActivity {
 
     private ListView listView;
     private BirthdayCursorAdapter adapter;
+    private TextView tvEmpty;
+    private RelativeLayout rl;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +37,26 @@ public class TodayBirthdayActivity extends AppCompatActivity {
                 getSystemService(NOTIFICATION_SERVICE);
         mNotificationManager.cancelAll();
 
-
         BirthdayDbQueries dbq = new BirthdayDbQueries(new BirthdayDbHelper(this));
         String selection = "strftime('%m-%d'," + BirthdayContract.BirthdayEntry.COLUMN_NAME_DATE + "/1000, 'unixepoch')" + " == strftime('%m-%d',?/1000, 'unixepoch')";
         String[] selectionArgs = {Long.toString(Calendar.getInstance().getTimeInMillis()- 86400000)};
-        Cursor cursor = dbq.read(DbColumns.columns, selection, selectionArgs, null, null, BirthdayContract.BirthdayEntry.COLUMN_NAME_NAME + " ASC");
+        cursor = dbq.read(DbColumns.columns, selection, selectionArgs, null, null, BirthdayContract.BirthdayEntry.COLUMN_NAME_NAME + " ASC");
 
+        rl = (RelativeLayout) findViewById(R.id.view_relative_layout);
         listView = (ListView) findViewById(R.id.today_birthday_list);
         adapter = new BirthdayCursorAdapter(this, cursor, 0);
         listView.setAdapter(adapter);
+
+        tvEmpty = rl.findViewById(R.id.today_empty_view);
+        listView.setEmptyView(tvEmpty);
+        tvEmpty.setText("No birthday record found today");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        adapter.swapCursor(cursor);
+        tvEmpty.setVisibility(View.GONE);
+        adapter.notifyDataSetChanged();
     }
 }
