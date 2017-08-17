db7ec958ae9789d7a161ddd7d500402f53b1f86f
Wan Ching <wanching_97@hotmail.com>
2017-08-17 17:44:38 +0800

    deleted unused import

--- app/src/main/java/com/wanching/birthdayreminder/Fragments/AddMEssageDialog.java
+++ app/src/main/java/com/wanching/birthdayreminder/Fragments/AddMEssageDialog.java
@@ -7,9 +7,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.view.ContextThemeWrapper;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.EditText;
 
 import com.wanching.birthdayreminder.R;
